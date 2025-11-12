ae011ef6d48969ce46fb14eb78f131980a2a510e
Marichka <maria_volynchuk@ukr.net>
2025-11-12 20:20:30 +0100

    resolved issues

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,13 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  const dailyPrice = 40;
+  const threeDayDiscount = 20;
+  const sevenDayDiscount = 50;
+
   if (days < 3) {
-    return days * 40;
-  } else if (days < 7) {
-    return days * 40 - 20;
-  } else {
-    return days * 40 - 50;
+    return days * dailyPrice;
   }
+
+  if (days < 7) {
+    return days * dailyPrice - threeDayDiscount;
+  }
+
+  return days * dailyPrice - sevenDayDiscount;
 }
 
 module.exports = calculateRentalCost;
