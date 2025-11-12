a18092b917b4548519c6c34a360a848eaa7cfd7f
Marichka <maria_volynchuk@ukr.net>
2025-11-12 20:23:48 +0100

    resolved issues

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,18 +5,20 @@
  */
 function calculateRentalCost(days) {
   const dailyPrice = 40;
-  const threeDayDiscount = 20;
-  const sevenDayDiscount = 50;
+  const smallDiscount = 20;
+  const bigDiscount = 50;
+  const smallDiscountTerm = 3;
+  const bigDiscountTerm = 7;
 
-  if (days < 3) {
+  if (days < smallDiscountTerm) {
     return days * dailyPrice;
   }
 
-  if (days < 7) {
-    return days * dailyPrice - threeDayDiscount;
+  if (days < bigDiscountTerm) {
+    return days * dailyPrice - smallDiscount;
   }
 
-  return days * dailyPrice - sevenDayDiscount;
+  return days * dailyPrice - bigDiscount;
 }
 
 module.exports = calculateRentalCost;
